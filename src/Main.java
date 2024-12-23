625d7f172d242af380f2d65ca506f8091ca828d8
jaba <jabagogoladzis3@gmail.com>
2024-12-23 15:35:00 +0400

    git testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hello world");
 
+        for(int i = 0; i<=10; i++) {
+            System.out.println("just repo testing " + i);
+        }
     }
 }
